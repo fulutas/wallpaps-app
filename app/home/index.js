051778ece6fad06f267454925d1db3de3d6ac2d9
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-04-18 17:37:18 +0300

    homescreen development

--- app/home/index.js
+++ app/home/index.js
@@ -1,12 +1,93 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable, ScrollView, TextInput } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { FontAwesome6, Feather, Ionicons } from '@expo/vector-icons';
+import { theme } from '../../constants/theme';
+import { hp, wp } from '../../helpers/common';
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+	const { top } = useSafeAreaInsets()
+	const paddingTop = top > 0 ? top + 10 : 30
+
+	const [search, setSearch] = useState("");
+	const searchInputRef = useRef(null)
+
+	return (
+		<View style={[styles.container, { paddingTop }]}>
+			<View style={styles.header}>
+				<Pressable>
+					<Text style={styles.title}>Wallpaps</Text>
+				</Pressable>
+				<Pressable>
+					<FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)} />
+				</Pressable>
+			</View>
+
+			<ScrollView contentContainerStyle={{ gap: 15 }}>
+				<View style={styles.searchBar}>
+					<View style={styles.searchIcon}>
+						<Feather name="search" size={24} color={theme.colors.neutral(0.4)} />
+					</View>
+					<TextInput
+						value={search}
+						onChangeText={value => setSearch(value)}
+						ref={searchInputRef}
+						placeholder='Search for photos...'
+						style={styles.searchInput}
+					/>
+					{search && (
+						<Pressable onPress={() => setSearch("")} style={styles.closeIcon}>
+							<Ionicons name="close" size={24} color={theme.colors.neutral(0.6)} />
+						</Pressable>
+					)}
+				</View>
+			</ScrollView>
+		</View >
+	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		gap: 15
+	},
+	header: {
+		marginHorizontal: wp(4),
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center"
+	},
+	title: {
+		fontSize: hp(4),
+		fontWeight: theme.fontWeights.semibold,
+		color: theme.colors.neutral(0.9)
+	},
+	searchBar: {
+		marginHorizontal: wp(4),
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		borderWidth: 1,
+		borderColor: theme.colors.grayBG,
+		backgroundColor: theme.colors.white,
+		padding: 6,
+		paddingLeft: 10,
+		borderRadius: theme.radius.lg
+	},
+	searchIcon: {
+		padding: 8
+	},
+	searchInput: {
+		flex: 1,
+		borderRadius: theme.radius.sm,
+		paddingVertical: 10,
+		fontSize: hp(1.8)
+	},
+	closeIcon: {
+		backgroundColor: theme.colors.neutral(0.1),
+		padding: 8,
+		borderRadius: theme.radius.sm
+	}
+})
 
 export default HomeScreen
