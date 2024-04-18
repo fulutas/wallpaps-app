7985f751cf1223d40f7a8b953d93bfb018defba7
Furkan Ulutaş <ulutasfurkan@gmail.com>
2024-04-18 18:11:27 +0300

    categories complete

--- app/home/index.js
+++ app/home/index.js
@@ -4,13 +4,19 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { FontAwesome6, Feather, Ionicons } from '@expo/vector-icons';
 import { theme } from '../../constants/theme';
 import { hp, wp } from '../../helpers/common';
+import Categories from '../../components/categories';
 
 const HomeScreen = () => {
 	const { top } = useSafeAreaInsets()
 	const paddingTop = top > 0 ? top + 10 : 30
 
 	const [search, setSearch] = useState("");
+	const [activeCategory, setActiveCategory] = useState(null)
 	const searchInputRef = useRef(null)
+
+	const handleChangeCategory = (cat) => {
+		setActiveCategory(cat)
+	}
 
 	return (
 		<View style={[styles.container, { paddingTop }]}>
@@ -40,6 +46,10 @@ const HomeScreen = () => {
 							<Ionicons name="close" size={24} color={theme.colors.neutral(0.6)} />
 						</Pressable>
 					)}
+				</View>
+
+				<View style={styles.categories}>
+					<Categories activeCategory={activeCategory} handleChangeCategory={handleChangeCategory} />
 				</View>
 			</ScrollView>
 		</View >
